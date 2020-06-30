ee8b2996360cbe7492e9e36e504375c017a7a73c
Oscar Turish <ortg_10@hotmail.com>
2020-06-30 15:01:30 -0600

    Se agrego prueba para conectarse a BD
    Se deben completar los valores URL USER Y PASSWORD

--- src/sistemapararrhh/Principal.java
+++ src/sistemapararrhh/Principal.java
@@ -5,17 +5,35 @@
  */
 package sistemapararrhh;
 
+import java.util.logging.Logger;
+import java.sql.Connection;
+import java.sql.DriverManager;
+
 /**
  *
  * @author ortg_
  */
 public class Principal {
 
+    private static final String URL = "";
+    private static final String USER = "";
+    private static final String PASSWORD = "";
+    private static final String DRIVER = "com.mysql.cj.jdbc.Driver";
+    private static Logger log = Logger.getLogger(Principal.class.getName());
     /**
      * @param args the command line arguments
      */
     public static void main(String[] args) {
         // TODO code application logic here
+        try {
+            Class.forName(DRIVER);
+            Connection con = DriverManager.getConnection(URL, USER, PASSWORD);
+            log.info("Se ha realizado la conexion a la BD");
+
+        } catch (Exception ex) {
+            ex.printStackTrace();
+        }
+
     }
-    
+
 }
