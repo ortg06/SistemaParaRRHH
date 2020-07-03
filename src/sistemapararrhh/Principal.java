ede513b37389196e7d62535e7fd2700681e080b2
Oscar Turish <ortg_10@hotmail.com>
2020-07-03 11:20:05 -0600

    Entidad Empleado y prueba de recuperar datos

--- src/sistemapararrhh/Principal.java
+++ src/sistemapararrhh/Principal.java
@@ -5,8 +5,11 @@
  */
 package sistemapararrhh;
 
+import com.mysql.cj.jdbc.PreparedStatementWrapper;
 import java.util.logging.Logger;
 import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
 import java.sql.DriverManager;
 import sistemarrhh.connections.ConnectionDB;
 
@@ -16,10 +19,6 @@ import sistemarrhh.connections.ConnectionDB;
  */
 public class Principal {
 
-    private static final String URL = "";
-    private static final String USER = "";
-    private static final String PASSWORD = "";
-    private static final String DRIVER = "com.mysql.cj.jdbc.Driver";
     private static Logger log = Logger.getLogger(Principal.class.getName());
     /**
      * @param args the command line arguments
@@ -28,9 +27,23 @@ public class Principal {
         // TODO code application logic here
         try {
             Connection con = ConnectionDB.openConnection();
-          
             log.info("Se ha realizado la conexion a la BD");
-
+            
+            PreparedStatement ps = con.prepareStatement("select * from GE_EM_EMPLEADO");
+            ResultSet rs = ps.executeQuery();
+            log.info("Obteniendo informacion");
+           
+            while(rs.next()){
+                System.out.println("Nombre Empleado:"+rs.getString("EM_NOMBRE"));
+            }
+            //cerrando objetos y conexion
+            log.info("Cerrando conexion");
+            rs.close();
+            ps.close();
+            con.close();
+            
+          
+            
         } catch (Exception ex) {
             ex.printStackTrace();
         }
--- src/sistemarrhh/entidades/Empleado.java
+++ src/sistemarrhh/entidades/Empleado.java
@@ -5,10 +5,125 @@
  */
 package sistemarrhh.entidades;
 
+import java.sql.Date;
+
 /**
  *
  * @author ortg_
  */
 public class Empleado {
+
+    private int noEmpleado;
+    private String nombre;
+    private String apellido;
+    private Date fechaNacimiento;
+    private Date fechaContrato;
+    private int dpID;
+    private int superior;
+    private int tpID;
+    private int stId;
+
+    public Empleado() {
+    }
+
+    public Empleado(int noEmpleado, String nombre, String apellido, Date fechaNacimiento, Date fechaContrato, int dpID, int superior, int tpID, int stId) {
+        this.noEmpleado = noEmpleado;
+        this.nombre = nombre;
+        this.apellido = apellido;
+        this.fechaNacimiento = fechaNacimiento;
+        this.fechaContrato = fechaContrato;
+        this.dpID = dpID;
+        this.superior = superior;
+        this.tpID = tpID;
+        this.stId = stId;
+    }
+
+    public Empleado(int noEmpleado, String nombre, String apellido, Date fechaNacimiento, Date fechaContrato, int dpID, int tpID, int stId) {
+        this.noEmpleado = noEmpleado;
+        this.nombre = nombre;
+        this.apellido = apellido;
+        this.fechaNacimiento = fechaNacimiento;
+        this.fechaContrato = fechaContrato;
+        this.dpID = dpID;
+        this.tpID = tpID;
+        this.stId = stId;
+    }
+
+    public int getNoEmpleado() {
+        return noEmpleado;
+    }
+
+    public void setNoEmpleado(int noEmpleado) {
+        this.noEmpleado = noEmpleado;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public String getApellido() {
+        return apellido;
+    }
+
+    public void setApellido(String apellido) {
+        this.apellido = apellido;
+    }
+
+    public Date getFechaNacimiento() {
+        return fechaNacimiento;
+    }
+
+    public void setFechaNacimiento(Date fechaNacimiento) {
+        this.fechaNacimiento = fechaNacimiento;
+    }
+
+    public Date getFechaContrato() {
+        return fechaContrato;
+    }
+
+    public void setFechaContrato(Date fechaContrato) {
+        this.fechaContrato = fechaContrato;
+    }
+
+    public int getDpID() {
+        return dpID;
+    }
+
+    public void setDpID(int dpID) {
+        this.dpID = dpID;
+    }
+
+    public int getSuperior() {
+        return superior;
+    }
+
+    public void setSuperior(int superior) {
+        this.superior = superior;
+    }
+
+    public int getTpID() {
+        return tpID;
+    }
+
+    public void setTpID(int tpID) {
+        this.tpID = tpID;
+    }
+
+    public int getStId() {
+        return stId;
+    }
+
+    public void setStId(int stId) {
+        this.stId = stId;
+    }
     
+    
+    
+    
+    
+
 }
