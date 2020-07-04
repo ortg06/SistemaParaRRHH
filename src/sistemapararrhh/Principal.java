bd30b2bd8417ba9fc05ed5f9d4f2a18b3fa459dc
Oscar Turish <ortg_10@hotmail.com>
2020-07-04 15:35:00 -0600

    Creacion de modulos Admon y RRHH

--- src/sistemapararrhh/Principal.java
+++ src/sistemapararrhh/Principal.java
@@ -5,7 +5,6 @@
  */
 package sistemapararrhh;
 
-
 import static java.lang.System.exit;
 import java.sql.SQLException;
 import java.util.logging.Logger;
@@ -13,11 +12,14 @@ import java.util.List;
 import java.util.Scanner;
 import java.util.logging.Level;
 import sistemarrhh.dao.EmpleadoDao;
+import sistemarrhh.dao.UsuarioDao;
 import sistemarrhh.entidades.Empleado;
 import sistemarrhh.entidades.Usuario;
 import sistemarrhh.utilidades.BuscarEmpleado;
 import sistemarrhh.utilidades.CargoForm;
 import sistemarrhh.utilidades.DepartamentoForm;
+import sistemarrhh.utilidades.EnviarCorreo;
+import sistemarrhh.utilidades.FuncionCodAleatorio;
 import sistemarrhh.utilidades.RegistroEmpleado;
 
 /**
@@ -36,21 +38,63 @@ public class Principal {
         Scanner sc = new Scanner(System.in);
         byte decision = 0;
         EmpleadoDao empleadoDao = new EmpleadoDao();
-        Usuario user = new Usuario();
-        
-        
+        UsuarioDao usuarioDao = new UsuarioDao();
+        int cod;
+        int num;
+        String contra;
+        Usuario user2 = new Usuario();
+
         System.out.println("\nInicio de Sesión.");//Muestra Inicio de Sesion Usuario- COntraseña
         System.out.println("\nIngrese codigo de Usuario: ");
-        user.setUsID(Integer.parseInt(sc.nextLine()));
+        cod = Integer.parseInt(sc.nextLine());
         System.out.println("\nContraseña: ");
-        user.(sc.nextLine());
-        user.setCorreo(CORREO);
-        
+        contra = sc.nextLine();
 
+        try {
+            user2 = usuarioDao.getByIDData(cod);
+        } catch (ClassNotFoundException ex) {
+            Logger.getLogger(Principal.class.getName()).log(Level.SEVERE, null, ex);
+        } catch (SQLException ex) {
+            Logger.getLogger(Principal.class.getName()).log(Level.SEVERE, null, ex);
+        }
+
+        //Verificamos si el codigo y la contraseña pertenecen al usuario
+        while (cod != user2.getUsID() || !user2.getUsContraseña().equals(contra)) {
+            System.out.println("Codigo de Usuario o Contraseña Incorrecto");
+            exit(0);
+        }
+        
+        /*
+        //Enviamos correo para doble factor de verificacion 
+        EnviarCorreo correo = new EnviarCorreo(); //Instanciamos la clase EnviarCorreo
+        num = FuncionCodAleatorio.getNumeroAleatorio(); //guardamos el numero aleatorio 
+        correo.enviarCodigo(user2, num);  //Llammos la funcion enviar codigo con los parametros user y num  aleatorio
+        System.out.println("\nPor razones de segurida te hemos enviado un codigo de acceso a tu correo");//mostramos mensaje para verificar la edad
+        System.out.println("\nIngresa tu codigo: ");
+        int codigo = Integer.parseInt(sc.nextLine());
+
+        if (codigo == num) { //Comprobamos que el codigo enviado al correo sea igual al generado aleatoriamente
+            System.out.println("\nRealizaste un Inicio de sesion seguro Bienvenido: " + user2.getUsNombre());
+        } else { //si no es el codigo se termina la sesion.
+            System.out.println("Lo sentimos, No ingresaste el codigo correcto");
+            exit(0); // Se termina la sesion 
+        }*/
+        
         System.out.println("\n-----BIENVENIDO AL SISTEMA DE RECURSOS HUMANOS-----\n");
 
-        byte flag = 0; //variable que permite mostrar de nuevo el menu.
+        if (user2.getRolID() == 1) {
+             System.out.println("\n--Usted posee un rol de Administrador--");
+             Administrador admon = new Administrador();
+             admon.gestionAdmon();
+        } else if (user2.getRolID() == 2) {
+             System.out.println("\n--Usted posee un rol de Recursos Humanos--");
+            RecursosHumano rrhh= new RecursosHumano();
+            rrhh.gestionRRHH();
+        }
 
+        
+        byte flag = 0; //variable que permite mostrar de nuevo el menu.
+/*
         while (flag == 0) {// while para controlar que se muestre el menu principal
             byte decision2 = 0;
 
@@ -70,7 +114,7 @@ public class Principal {
                 System.out.println("2. Consultar informacion de un empleado");
                 System.out.println("3. Ingresar un nuevo empleado");
                 System.out.println("4. Gestion de departamentos");
-                System.out.println("5. GEstion de Cargos");
+                System.out.println("5. Gestion de Cargos");
                 System.out.println("6. Salir");
                 decision = Byte.parseByte(sc.nextLine());
             }
@@ -78,14 +122,14 @@ public class Principal {
             switch (decision2) { //Switch que controla el acceso a las opciones del menu
                 case 1:
                     try {
-                       
+
                         List<Empleado> empleados = empleadoDao.getAllData();
                         for (Empleado e : empleados) {
                             System.out.println(e);
                         }
-                       /* log.info("El nombre actual es: " + e.getNombre());
+                        /*log.info("El nombre actual es: " + e.getNombre());
                         e.setNombre("Oscar");
-                        empleadoDao.updateData(e);*/
+                        empleadoDao.updateData(e);
 
                     } catch (Exception ex) {
                         ex.printStackTrace();
@@ -97,47 +141,47 @@ public class Principal {
                     break;
                 case 3:
                     RegistroEmpleado registro = new RegistroEmpleado();
-                {
-                    try {
-                        registro.Registro();
-                    } catch (SQLException ex) {
-                        Logger.getLogger(Principal.class.getName()).log(Level.SEVERE, null, ex);
-                    } catch (ClassNotFoundException ex) {
-                        Logger.getLogger(Principal.class.getName()).log(Level.SEVERE, null, ex);
+                     {
+                        try {
+                            registro.Registro();
+                        } catch (SQLException ex) {
+                            Logger.getLogger(Principal.class.getName()).log(Level.SEVERE, null, ex);
+                        } catch (ClassNotFoundException ex) {
+                            Logger.getLogger(Principal.class.getName()).log(Level.SEVERE, null, ex);
+                        }
                     }
-                }
                     break;
-                    
-                case 4:  
+
+                case 4:
                     DepartamentoForm nuevoDepto = new DepartamentoForm();
-                {
-                    try {
-                        nuevoDepto.nuevoDepto();
-                    } catch (SQLException ex) {
-                        Logger.getLogger(Principal.class.getName()).log(Level.SEVERE, null, ex);
-                    } catch (ClassNotFoundException ex) {
-                        Logger.getLogger(Principal.class.getName()).log(Level.SEVERE, null, ex);
+                     {
+                        try {
+                            nuevoDepto.nuevoDepto();
+                        } catch (SQLException ex) {
+                            Logger.getLogger(Principal.class.getName()).log(Level.SEVERE, null, ex);
+                        } catch (ClassNotFoundException ex) {
+                            Logger.getLogger(Principal.class.getName()).log(Level.SEVERE, null, ex);
+                        }
                     }
-                }
                     break;
                 case 5:
                     CargoForm gestionCargo = new CargoForm();
-                {
-                    try {
-                        gestionCargo.nuevoCargo();
-                    } catch (SQLException ex) {
-                        Logger.getLogger(Principal.class.getName()).log(Level.SEVERE, null, ex);
-                    } catch (ClassNotFoundException ex) {
-                        Logger.getLogger(Principal.class.getName()).log(Level.SEVERE, null, ex);
+                     {
+                        try {
+                            gestionCargo.nuevoCargo();
+                        } catch (SQLException ex) {
+                            Logger.getLogger(Principal.class.getName()).log(Level.SEVERE, null, ex);
+                        } catch (ClassNotFoundException ex) {
+                            Logger.getLogger(Principal.class.getName()).log(Level.SEVERE, null, ex);
+                        }
                     }
-                }
-                    break;    
-    
+                    break;
+
                 case 6:
                     System.out.println("Adios...");
                     exit(0); //Se termina la sesion del sistema
                     break;
-            }
+            }*/
             System.out.println("\n¿Desea volver al menu principal? \n1:SI  2:Salir"); //Opciones para mostrar de nuevo el menu 
             flag = Byte.parseByte(sc.nextLine());
             while (flag != 1 && flag != 2) { //Validamos que el usuario seleccione una opcion valida (1 o 2)
@@ -152,8 +196,6 @@ public class Principal {
                     exit(0); //Sino terminamos la sesion
                     break;
             }
-        }
-
     }
 
 }
